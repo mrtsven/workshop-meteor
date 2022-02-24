8d20153ea80506759947fc21258e74f84d8bf850
Sven Zahharov <zahharov@hotmail.com>
2022-02-24 09:51:31 +0100

    7.2

--- server/main.js
+++ server/main.js
@@ -1,9 +1,19 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 import { TasksCollection } from '/imports/api/TasksCollection';
+
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
 
 const insertTask = taskText => TasksCollection.insert({ text: taskText });
 
 Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
   if (TasksCollection.find().count() === 0) {
     [
       'First Task',
